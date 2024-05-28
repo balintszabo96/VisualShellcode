aabf3b652ef9f31e94ae72a0cd3020437b1605d6
CTFBOX\balin <balint.22.szabo@gmail.com>
2024-05-28 21:24:18 +0300

    Added latest forgotten changes

--- BddisasmIntegration/UnitTests/UnitTests.cpp
+++ BddisasmIntegration/UnitTests/UnitTests.cpp
@@ -37,6 +37,9 @@ namespace UnitTests
 			unsigned int shemuStatus = 0;
 			uint64_t flags = 0;
 			int status = AnalyzeShellcode(windows_x64_exec_calc, sizeof(windows_x64_exec_calc), false, &shemuStatus, &flags);
+			Assert::IsTrue(status == static_cast<int>(Status::Success));
+			Assert::IsTrue(shemuStatus == SHEMU_ABORT_GLA_OUTSIDE);
+			Assert::IsTrue(flags == 10);
 			Assert::IsTrue(flags & SHEMU_FLAG_LOAD_RIP);
 			Assert::IsTrue(flags & SHEMU_FLAG_TIB_ACCESS);
 		}
--- test/tester.py
+++ test/tester.py
@@ -2,6 +2,15 @@ import ctypes
 
 handle = ctypes.WinDLL(r"b:\projects\VisualShellcode\BddisasmIntegration\x64\Release\BddisasmIntegration.dll")
 print(handle.Dummy)
-buff = ctypes.create_string_buffer(b"Hello", 10)
-handle.Dummy.argtypes = [ctypes.c_char_p]
-handle.Dummy(buff)
+with open(r'b:\projects\VisualShellcode\test\shellcodes\exec_calc_x64', 'rb') as f:
+    shellcode = f.read()
+buff = ctypes.create_string_buffer(shellcode, len(shellcode))
+shemuStatus = ctypes.c_uint32(0)
+shemuStatusPtr = ctypes.pointer(shemuStatus)
+flags = ctypes.c_uint64(0)
+flagsPtr = ctypes.pointer(flags)
+result = ctypes.c_int32(0)
+handle.ExpAnalyzeShellcode.argtypes = [ctypes.c_char_p, ctypes.c_int32, ctypes.c_bool, ctypes.POINTER(ctypes.c_uint32), ctypes.POINTER(ctypes.c_uint64)]
+handle.ExpAnalyzeShellcode.restype = ctypes.c_int32
+result = handle.ExpAnalyzeShellcode(buff, len(shellcode), False, shemuStatusPtr, flagsPtr)
+print("here")
