fe02b0fe35ee8117f556e4d681c9edc805c6eb0a
CTFBOX\balin <balint.22.szabo@gmail.com>
2024-06-03 20:32:13 +0300

    Added extra styling

--- webapp/app.py
+++ webapp/app.py
@@ -3,20 +3,19 @@ from utils.logic import get_disasm
 
 app = Flask(__name__)
 
-def process(input_text):
-    # Here, you can process the input text in any way you like.
-    # For simplicity, let's just reverse the text.
-    #return input_text.replace(' ', '')
-    disasm = get_disasm(input_text.replace(' ', '')).split('\n')
+def process(input_text, architecture):
+    disasm = get_disasm(input_text.replace(' ', ''), architecture).split('\n')
     return disasm
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     processed_text = ""
+    submitted_text = ""
     if request.method == 'POST':
         submitted_text = request.form['text_input']
-        processed_text = process(submitted_text)
-    return render_template('index.html', processed_text=processed_text)
+        architecture = request.form['architecture']
+        processed_text = process(submitted_text, architecture)
+    return render_template('index.html', submitted_text=submitted_text, processed_text=processed_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- webapp/utils/logic.py
+++ webapp/utils/logic.py
@@ -1,10 +1,8 @@
 import ctypes
 
-def get_disasm(input):
+def get_disasm(input, architecture):
     shellcode = bytes.fromhex(input)
     buff = ctypes.create_string_buffer(shellcode, len(shellcode))
-    shemuStatus = ctypes.c_uint32(0)
-    shemuStatusPtr = ctypes.pointer(shemuStatus)
     handle = ctypes.WinDLL(r".\BddisasmIntegration\x64\Release\BddisasmIntegration.dll")
 
     result = ctypes.c_int32(0)
@@ -12,5 +10,5 @@ def get_disasm(input):
     disasm = ctypes.create_string_buffer(bufsize)
     handle.ExpDisasmShellcode.argtypes = [ctypes.c_char_p, ctypes.c_int32, ctypes.c_bool, ctypes.c_char_p, ctypes.c_uint64]
     handle.ExpDisasmShellcode.restype = ctypes.c_int32
-    result = handle.ExpDisasmShellcode(buff, len(shellcode), False, disasm, bufsize)
+    result = handle.ExpDisasmShellcode(buff, len(shellcode), architecture == 'x86', disasm, bufsize)
     return disasm.value.decode()
