3077cebfe4475bd5ff2b5be1ae83916168e401f9
CTFBOX\balin <balint.22.szabo@gmail.com>
2024-06-11 18:27:15 +0300

    Made the webapp plugin agnostic

--- webapp/app.py
+++ webapp/app.py
@@ -1,27 +1,29 @@
 from flask import Flask, render_template, request
-from utils.logic import get_disasm, get_emulation
-from utils.flag import Flag
+import sys
+from utils.PluginManager import PluginManager
 
 app = Flask(__name__)
+manager = PluginManager()
 
 def process(input_text, architecture):
     preprocessed_text = input_text.replace(' ', '')
-    instructions = get_disasm(preprocessed_text, architecture)
-    flags, passed_instructions = get_emulation(preprocessed_text, architecture)
-    return instructions, flags, passed_instructions
+    pluginResults = manager.runPlugins(preprocessed_text, architecture)
+    return pluginResults
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    instructions = ""
-    flags = ""
     submitted_text = ""
-    passed_instructions = ""
+    pluginResults = ""
     if request.method == 'POST':
         submitted_text = request.form['text_input']
         architecture = request.form['architecture']
-        instructions, flags, passed_instructions = process(submitted_text, architecture)
-    return render_template('index.html', submitted_text=submitted_text, instructions=instructions, flags=flags, passed_instructions=passed_instructions)
+        pluginResults = process(submitted_text, architecture)
+    return render_template('index.html', 
+                           submitted_text=submitted_text, 
+                           pluginResults=pluginResults,
+                           plugins_to_include = manager.getHtmlFiles())
 
 if __name__ == '__main__':
+    manager.loadPlugins()
     app.run(debug=True)
     #app.run(host='192.168.100.66', port=5000, debug=True)
